e3928503c21fd3eb663c80e82d157577a758ad1b
ねずみにうむ <nezumi.dev.0627@gmail.com>
2025-07-16 14:33:59 +0900

    add else

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -4,12 +4,17 @@ import UIKit
 private let _dylibLoaded: Void = {
     DispatchQueue.main.async {
         // iOS 13 以降の WindowScene 対応
-        let rootVC = UIApplication.shared
-            .connectedScenes
-            .compactMap { $0 as? UIWindowScene }
-            .flatMap { $0.windows }
-            .first(where: { $0.isKeyWindow })?
-            .rootViewController
+        let rootVC: UIViewController?
+        if #available(iOS 13.0, *) {
+            rootVC = UIApplication.shared
+                .connectedScenes
+                .compactMap { $0 as? UIWindowScene }
+                .flatMap { $0.windows }
+                .first(where: { $0.isKeyWindow })?
+                .rootViewController
+        } else {
+            rootVC = UIApplication.shared.keyWindow?.rootViewController
+        }
 
         if let rootVC = rootVC {
             let alert = UIAlertController(title: "[MyLibrary]", message: "dylib loaded", preferredStyle: .alert)
