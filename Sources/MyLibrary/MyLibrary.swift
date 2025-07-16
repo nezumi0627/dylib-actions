2a68cd63a0a461b52d0dcb3e42c4bcad6df6cade
ねずみにうむ <nezumi.dev.0627@gmail.com>
2025-07-16 14:47:46 +0900

    fix

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,32 +1,10 @@
 import Foundation
-import UIKit
 
 private let _dylibLoaded: Void = {
-    DispatchQueue.main.async {
-        // iOS 13 以降の WindowScene 対応
-        let rootVC: UIViewController?
-        if #available(iOS 13.0, *) {
-            rootVC = UIApplication.shared
-                .connectedScenes
-                .compactMap { $0 as? UIWindowScene }
-                .flatMap { $0.windows }
-                .first(where: { $0.isKeyWindow })?
-                .rootViewController
-        } else {
-            rootVC = UIApplication.shared.keyWindow?.rootViewController
-        }
-
-        if let rootVC = rootVC {
-            let alert = UIAlertController(title: "[MyLibrary]", message: "dylib loaded", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default))
-            rootVC.present(alert, animated: true)
-        } else {
-            fputs("[MyLibrary] dylib loaded (no root ViewController found)\n", stderr)
-        }
-    }
+    fputs("[MyLibrary] dylib loaded\n", stderr)
 }()
 
-public func greet() {
+@_cdecl("greet") public func greet() {
     _ = _dylibLoaded
     print("Hello from MyLibrary!")
 }
