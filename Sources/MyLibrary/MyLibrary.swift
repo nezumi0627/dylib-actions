c9beeeaf6cecca2e6bda66e37292f0c2f8fba989
ねずみにうむ <nezumi.dev.0627@gmail.com>
2025-07-16 14:53:25 +0900

    fix

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,10 +1,18 @@
-import Foundation
+import UIKit
 
 private let _dylibLoaded: Void = {
-    fputs("[MyLibrary] dylib loaded\n", stderr)
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let rootVC = windowScene.windows.first(where: { $0.isKeyWindow })?.rootViewController {
+
+            let alert = UIAlertController(title: "[MyLibrary]", message: "dylib がロードされました", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "おけ", style: .default))
+            rootVC.present(alert, animated: true)
+        }
+    }
 }()
 
-@_cdecl("greet") public func greet() {
+public func greet() {
     _ = _dylibLoaded
     print("Hello from MyLibrary!")
 }
